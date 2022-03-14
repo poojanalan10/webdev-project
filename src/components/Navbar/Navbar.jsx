7d41f74fad8a7027593e1dec289081c0e073e2a8
poojanalan10 <pnalan@pdx.edu>
2022-03-14 15:25:28 -0700

    updated nav bar and responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,46 @@
 import "./Navbar.css"
-import React, {Component} from 'react'
+import React from 'react'
+import { useState,useEffect } from "react";
 import { MenuItems} from "./MenuItems";
+const NavBar = () => { 
 
-class NavBar extends Component {
+    const [toggleNavigationBar, settoggleNavigationBar] = useState(false);
+    const [screeWidth, setScreenWidth] = useState(window.innerWidth)
+   
+    const toggleNav = () => {
+        settoggleNavigationBar(!toggleNavigationBar)
+    }
 
-    render() {
+    useEffect(() =>{
+        const changeWidth =()=>{
+            setScreenWidth(window.innerWidth);
+
+        }
+        window.addEventListener('resize',changeWidth)
+        return() => {
+            window.removeEventListener('resize',changeWidth)
+        }
+    }, [])
     return (
         <nav className="navbar">
-
-            <ul className= 'nav-menu'>
-                {MenuItems.map((item,index) =>{
-                    return (    
-                        <li key={index}>
-                            <a className={item.cname} href={item.url} >
-                                {item.title}
-                            </a>
-                        </li>
-                    )
-                } )}    
-            </ul>
+            {(toggleNavigationBar || screeWidth > 600)&& (
+                 <ul className= 'nav-menu'>
+                 {MenuItems.map((item,index) =>{
+                     return (    
+                         <li key={index}>
+                             <a className={item.cname} href={item.url} >
+                                 {item.title}
+                             </a>
+                         </li>
+                     )
+                 } )}    
+             </ul>
+            )}
+            <button className="btn" onClick={toggleNav}>Menu</button>
+            
+           
         </nav>
     );
-    }
+    
 }
 export default NavBar
--- src/components/introduction/intro.jsx
+++ src/components/introduction/intro.jsx
@@ -1,5 +1,4 @@
 import "./intro.css"
-import Me from "../../img/cs.png"
 
 const Intro = () => {
     return (
@@ -15,14 +14,10 @@ const Intro = () => {
                             <div className="i-title-item"> Administrator </div>
                         </div>
                     </div>
-                    <p className="i-desc">I'm a software developer and aspire to work in software development. I have worked as a software
+                    <div className="i-desc">I'm a software developer and aspire to work in software development. I have worked as a software
                     tester which is an important quality in developing a software with fewer bugs. I've also done a few administrative jobs 
-                    which requires great attention to details.</p>
+                    which requires great attention to details.</div>
                 </div>    
-            </div>
-            <div className="i-right">
-                <div className="i-bg"></div>
-                <img src={Me} alt="girl" className="i-img"/>
             </div>
         </div>
     );

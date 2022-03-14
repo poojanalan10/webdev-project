09fe3663323034a7f7354f5f605c1f4b029bbf4a
poojanalan10 <pnalan@pdx.edu>
2022-03-13 23:52:58 -0700

    font size updates to Projects

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -14,18 +14,18 @@ const About = () => {
                 <h1 className="a-title"> About Me </h1>
                 <p className="a-sub">
                 "An investment in knowledge pays the best interest."
-                                            - Benjamin Franklin
+                                        
                 </p>
                 <p className="a-desc">
                 Being a computer science masters student I have had the opportnity
                 to learn and execute various concepts of programming and analytics 
                 that widened my perspective. I have learnt how to gather, analyze, 
-                filter, combine and represent data in the desired format using database managemnt
+                filter, combine and represent data in the desired format using database management
                 systems. And, also have my hands on some basic concepts in networking to a few
                 advanced programming in java.
                 
                 I also have a basic understanding of web technologies. This wide array of learning
-                has helped me a long way in my masters journey and the thirst for knowledge will continue
+                has helped me a long way in my masters journey and the thirst for knowledge will continue,
                 as the only constant is change when it comes to technology.
                 </p>
             </div>

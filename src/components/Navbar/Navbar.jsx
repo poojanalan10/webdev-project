47ae1883ae24a86d9eb70ed09a95b53cc69599d8
poojanalan10 <pnalan@pdx.edu>
2022-03-13 22:23:39 -0700

    responsivenss for width greater than 1000

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,17 +3,12 @@ import React, {Component} from 'react'
 import { MenuItems} from "./MenuItems";
 
 class NavBar extends Component {
-    state={clicked:false}
-    handleClick = () =>{
-      this.setState({clicked: !this.state.clicked})
-    }
+
     render() {
     return (
         <nav className="navbar">
-            <div className="menu-icon" onClick={this.handleClick}>
-                <i className={this.state.clicked? 'fas fa-times' : 'fas fa-bars'}></i>
-            </div>
-            <ul className={this.state.clicked ? 'nav-menu active':'nav-menu'}>
+
+            <ul className= 'nav-menu'>
                 {MenuItems.map((item,index) =>{
                     return (    
                         <li key={index}>
